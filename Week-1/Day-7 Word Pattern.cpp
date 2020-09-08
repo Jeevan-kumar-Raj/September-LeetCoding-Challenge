883feb56dc65b2c987fc70235bc1c76dd7056717
Jeevan Kumar <63276441+Jeevan-kumar-Raj@users.noreply.github.com>
2020-09-08 23:42:36 +0530

    Add files via upload

---
+++ Week-1/Day-7 Word Pattern.cpp
@@ -0,0 +1,74 @@
+Day-7 Word Pattern.cpp
+
+
+/*
+Given a pattern and a string str, find if str follows the same pattern.
+
+Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in str.
+
+Example 1:
+
+Input: pattern = "abba", str = "dog cat cat dog"
+Output: true
+Example 2:
+
+Input:pattern = "abba", str = "dog cat cat fish"
+Output: false
+Example 3:
+
+Input: pattern = "aaaa", str = "dog cat cat dog"
+Output: false
+Example 4:
+
+Input: pattern = "abba", str = "dog dog dog dog"
+Output: false
+Notes:
+You may assume pattern contains only lowercase letters, and str contains lowercase letters that may be separated by a single space.
+
+
+
+
+*/
+
+
+// Time:  O(n)
+// Space: O(c), c is unique count of pattern
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string str) {
+        // Count the words.
+        int cnt = str.empty() ? 0 : 1;
+        for (const auto& c : str) {
+            if (c == ' ') {
+                ++cnt;
+            }
+        }
+        if (pattern.size() != cnt) {
+            return false;
+        }
+
+        unordered_map<string, char> w2p;
+        unordered_map<char, string> p2w;
+        int i = 0, j = 0;
+        for (const auto& p : pattern) {
+            // Get a word at a time without saving all the words.
+            j = str.find(" ", i);
+            if (j == string::npos) {
+                j = str.length();
+            }
+            const string w = str.substr(i, j - i);
+    
+            if (!w2p.count(w) && !p2w.count(p)) { 
+                // Build mapping. Space: O(c)
+                w2p[w] = p; 
+                p2w[p] = w; 
+            } else if (!w2p.count(w) || w2p[w] != p) {
+                // Contradict mapping.
+                return false;
+            }
+            i = j + 1;
+        }
+        return true;
+    }
+};
